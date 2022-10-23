abebd0debcf1e2e90ee9081ee1659e03cfb9fbbd
Soren Lassen <sorenlassen@gmail.com>
2022-10-23 08:53:52 -0700

    rm DisposableElementsAttrBase
    Signed-off-by: Soren Lassen <sorenlassen@gmail.com>

--- src/Dialect/ONNX/ONNXAttributes.hpp
+++ src/Dialect/ONNX/ONNXAttributes.hpp
@@ -12,32 +12,35 @@
 
 #include "src/Dialect/ONNX/ONNXDialect.hpp"
 
-namespace mlir {
-class DisposableElementsAttr;
-}
+#include <memory>
 
 namespace onnx_mlir {
 
-struct DisposableElementsImpl {};
+struct DisposableElementsImpl {
+  mlir::ShapedType type;
+};
 using DisposableElements = std::shared_ptr<DisposableElementsImpl>;
+
 class DisposableExpression {
 public:
   DisposableElements getResult(size_t i);
 };
 using DisposableResultsHandle = DisposableExpression *;
-class DisposableElementsAttrBase : public mlir::Attribute {
-public:
-  using mlir::Attribute::Attribute;
-
-  /// Allow implicit conversion to ElementsAttr.
-  operator mlir::ElementsAttr() const {
-    return *this ? cast<mlir::ElementsAttr>() : nullptr;
-  }
-private:
-  mlir::DisposableElementsAttr *getAttr();
-};
 
 }
 
 #define GET_ATTRDEF_CLASSES
 #include "src/Dialect/ONNX/ONNXAttributes.hpp.inc"
+
+namespace mlir {
+
+template <typename T>
+inline auto DisposableElementsAttr::value_begin_impl(OverloadToken<Attribute>) const {
+  return nullptr;
+}
+
+inline ::onnx_mlir::DisposableElements DisposableElementsAttr::getElements() const {
+  return getResultsHandle()->getResult(getResultIndex());
+}
+
+}
