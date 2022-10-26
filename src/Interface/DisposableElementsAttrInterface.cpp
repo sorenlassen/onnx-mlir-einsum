9ada5a13c31d7b708fb294ba9db3334c9ded6c37
Soren Lassen <sorenlassen@gmail.com>
2022-10-26 10:02:55 -0700

    DisposableElementsAttrInterface
    Signed-off-by: Soren Lassen <sorenlassen@gmail.com>

---
+++ src/Interface/DisposableElementsAttrInterface.cpp
@@ -0,0 +1,19 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+//===---------------- DisposableElementsAttrInterface.cpp -----------------===//
+//
+// Defines some methods shared by all DisposableElementsAttr<T> template
+// instantiations.
+//
+//===----------------------------------------------------------------------===//
+
+#include "src/Interface/DisposableElementsAttrInterface.hpp"
+
+namespace mlir {
+
+/// Include the auto-generated declarations.
+#include "src/Interface/DisposableElementsAttrInterface.cpp.inc"
+
+} // end namespace mlir
