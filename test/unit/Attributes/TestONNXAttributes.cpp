733cd13e3718f6a27942c4c81d64f364da0f442d
Soren Lassen <sorenlassen@gmail.com>
2022-11-03 21:30:05 -0700

    test comments about iterator limitations
    Signed-off-by: Soren Lassen <sorenlassen@gmail.com>

--- test/unit/Attributes/TestONNXAttributes.cpp
+++ test/unit/Attributes/TestONNXAttributes.cpp
@@ -363,6 +363,13 @@ public:
     auto api = *apbegin;
     assert(api.getZExtValue() == 7);
 
+    auto apfbegin = i.value_begin<APFloat>();
+    // auto apf = *apfbegin; // crashes because ints refuse to cast to APFloat
+    (void)apfbegin;
+
+    // iteration by attributes is not supported
+    // auto atbegin = i.value_begin<IntegerAttr>(); // crashes
+
     ElementsAttr e = i; // i.cast<ElementsAttr>();
     t = e.getType();
     assert(!e.isSplat());
