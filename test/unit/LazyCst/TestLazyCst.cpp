4c854c77a6775d38a0efc5b8dc677a354526d6a8
Soren Lassen <slassen@groq.com>
2023-08-13 14:30:35 -0700

    configure lazyDialect.fileDataManager
    Signed-off-by: Soren Lassen <slassen@groq.com>

--- test/unit/LazyCst/TestLazyCst.cpp
+++ test/unit/LazyCst/TestLazyCst.cpp
@@ -33,35 +33,50 @@ public:
   Test(MLIRContext *ctx) : ctx(ctx), loc(UnknownLoc::get(ctx)), b(ctx) {
     F32 = b.getF32Type();
     I32 = b.getI32Type();
+
+    auto &lazyDialect = *ctx->getLoadedDialect<lazycst::LazyCstDialect>();
+    lazycst::FileDataManager::Config config;
+    config.readDirectoryPaths.push_back(".");
+    lazyDialect.fileDataManager.configure(config);
+  }
+
+  llvm::raw_ostream &print_ea(const std::string &name, ElementsAttr ea) {
+    return llvm::outs() << name << "=" << ea << ":" << ea.getShapedType();
   }
 
   int test_file_data() {
-    auto print = [](const std::string &name, ElementsAttr ea) {
-      llvm::outs() << name << "=" << ea << ":" << ea.getShapedType() << "\n";
-    };
-
     auto type = RankedTensorType::get({5}, F32);
+    // TODO: pre-populate foo.data
     auto path0 = b.getStringAttr("foo.data");
     auto f0 = FileDataElementsAttr::get(type, path0);
     auto path1 = b.getStringAttr("bar.data");
     auto f1 = FileDataElementsAttr::get(type, path1, 1);
-    print("f0", f0);
-    print("f1", f1);
+    print_ea("f0", f0) << "\n";
+    print_ea("f1", f1) << "\n";
+
     ArrayRef<char> rawBytes = f0.getRawBytes();
     std::cout << "f0.getRawBytes() size=" << rawBytes.size() << " [0]='"
               << rawBytes[0] << "'\n";
 
+    auto m = ModuleOp::create(loc);
+    m->setAttr("f0", f0);
+    m->setAttr("f1", f1);
+
+    llvm::outs() << m << "\n";
+    return 0;
+  }
+
+  int test_lazy_elements() {
+    auto type = RankedTensorType::get({5}, F32);
     auto d = DenseElementsAttr::get<float>(type, 3.14f);
     auto neg = b.getStringAttr("neg");
     auto e0 = LazyElementsAttr::get(type, neg, {d});
     auto add = b.getStringAttr("add");
     auto e1 = LazyElementsAttr::get(type, add, {e0, e0});
-    print("e0", e0);
-    print("e1", e1);
+    print_ea("e0", e0) << "n";
+    print_ea("e1", e1) << "n";
 
     auto m = ModuleOp::create(loc);
-    m->setAttr("f0", f0);
-    m->setAttr("f1", f1);
     m->setAttr("e0", e0);
     m->setAttr("e1", e1);
 
@@ -78,6 +93,7 @@ int main(int argc, char *argv[]) {
   Test test(&context);
   int failures = 0;
   failures += test.test_file_data();
+  failures += test.test_lazy_elements();
   if (failures != 0) {
     std::cerr << failures << " test failures\n";
     return 1;
