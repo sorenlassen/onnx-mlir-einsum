8c5add71c46402b1bc9fdabc2dae649f5bf08423
Soren Lassen <slassen@groq.com>
2023-08-14 04:39:00 -0700

    redo LazyElementsAttr
    Signed-off-by: Soren Lassen <slassen@groq.com>

--- test/unit/LazyCst/TestLazyCst.cpp
+++ test/unit/LazyCst/TestLazyCst.cpp
@@ -3,7 +3,10 @@
  */
 
 #include "src/Dialect/LazyCst/LazyCst.hpp"
+#include "src/Dialect/LazyCst/LazyCstOps.hpp"
 
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/BuiltinTypes.h"
@@ -19,8 +22,7 @@ class Test {
   MLIRContext *ctx [[maybe_unused]];
   Location loc;
   OpBuilder b;
-  Type F32;
-  Type I32;
+  Type F32, I32, UI32;
 
   Attribute zero(Type t) {
     if (isa<FloatType>(t))
@@ -33,6 +35,7 @@ public:
   Test(MLIRContext *ctx) : ctx(ctx), loc(UnknownLoc::get(ctx)), b(ctx) {
     F32 = b.getF32Type();
     I32 = b.getI32Type();
+    UI32 = b.getIntegerType(32, /*isSigned=*/false);
 
     auto &lazyDialect = *ctx->getLoadedDialect<lazycst::LazyCstDialect>();
     lazycst::FileDataManager::Config config;
@@ -67,6 +70,7 @@ public:
   }
 
   int test_lazy_elements() {
+#if 0
     auto type = RankedTensorType::get({5}, F32);
     auto d = DenseElementsAttr::get<float>(type, 3.14f);
     auto neg = b.getStringAttr("neg");
@@ -81,6 +85,53 @@ public:
     m->setAttr("e1", e1);
 
     llvm::outs() << m << "\n";
+#endif
+    return 0;
+  }
+
+  int test_lazy_func() {
+    auto f32type = RankedTensorType::get({5}, F32);
+    auto ui32type = RankedTensorType::get({5}, UI32);
+
+    auto m = ModuleOp::create(loc);
+    SymbolTable symbolTable(m);
+
+    b.setInsertionPointToStart(m.getBody());
+    FunctionType ftype = b.getFunctionType({}, {ui32type});
+    func::FuncOp f = func::FuncOp::create(loc, "f", ftype, {});
+    symbolTable.insert(f);
+    auto fblock = f.addEntryBlock();
+
+    b.setInsertionPointToStart(fblock);
+    auto d = DenseElementsAttr::get<float>(f32type, 3.14f);
+    auto fcstOp = b.create<arith::ConstantOp>(loc, d);
+    auto fcastOp = b.create<arith::FPToUIOp>(loc, ui32type, fcstOp);
+    b.create<func::ReturnOp>(loc, fcastOp.getResult());
+
+    b.setInsertionPointToStart(m.getBody());
+    constexpr char sym_name[] = "cstexpr0";
+    auto lazyElms =
+        LazyElementsAttr::get(ui32type, FlatSymbolRefAttr::get(ctx, sym_name));
+    FunctionType function_type = b.getFunctionType({f32type}, {ui32type});
+    auto arg_op_names =
+        b.getArrayAttr({OperationNameAttr::get(fcstOp->getName())});
+    auto res_op_names =
+        b.getArrayAttr({OperationNameAttr::get(*RegisteredOperationName::lookup(
+            arith::FPToUIOp::getOperationName(), ctx))});
+    auto arg_attrs = b.getArrayAttr({fcstOp->getAttrDictionary()});
+    auto res_attrs = b.getArrayAttr(
+        {b.getDictionaryAttr({b.getNamedAttr("value", lazyElms)})});
+    auto cexpr0 = b.create<LazyFuncOp>(loc, sym_name, function_type,
+        arg_op_names, res_op_names, arg_attrs, res_attrs);
+    SymbolTable(m).insert(cexpr0);
+    auto block = cexpr0.addEntryBlock();
+
+    b.setInsertionPointToStart(block);
+    auto castOp =
+        b.create<arith::FPToUIOp>(loc, ui32type, block->getArgument(0));
+    b.create<LazyReturnOp>(loc, ValueRange{castOp});
+
+    llvm::outs() << m << "\n";
     return 0;
   }
 };
@@ -89,11 +140,14 @@ public:
 
 int main(int argc, char *argv[]) {
   MLIRContext context;
+  context.loadDialect<arith::ArithDialect>();
+  context.loadDialect<func::FuncDialect>();
   context.loadDialect<LazyCstDialect>();
   Test test(&context);
   int failures = 0;
   failures += test.test_file_data();
   failures += test.test_lazy_elements();
+  failures += test.test_lazy_func();
   if (failures != 0) {
     std::cerr << failures << " test failures\n";
     return 1;
