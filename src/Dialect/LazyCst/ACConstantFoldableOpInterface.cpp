266c3226096515a236b7b1d9227d34b474c9cbaf
Soren Lassen <slassen@groq.com>
2023-09-06 11:29:52 -0700

    ACConstantFoldableOpInterface.td
    Signed-off-by: Soren Lassen <slassen@groq.com>

---
+++ src/Dialect/LazyCst/ACConstantFoldableOpInterface.cpp
@@ -0,0 +1,7 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "src/Dialect/LazyCst/ACConstantFoldableOpInterface.hpp"
+
+#include "src/Dialect/LazyCst/ACConstantFoldableOpInterface.cpp.inc"
