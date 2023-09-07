12002b91016d1bda64f6c903e7f1895d5827826d
Soren Lassen <slassen@groq.com>
2023-09-06 19:00:51 -0700

    LazyFunctionManager::getResults() - WIP
    Signed-off-by: Soren Lassen <slassen@groq.com>

--- src/Dialect/LazyCst/LazyFunctionManager.cpp
+++ src/Dialect/LazyCst/LazyFunctionManager.cpp
@@ -3,6 +3,7 @@
  */
 
 #include "src/Dialect/LazyCst/LazyFunctionManager.hpp"
+#include "src/Dialect/LazyCst/LazyCstOps.hpp"
 
 #include "mlir/IR/SymbolTable.h"
 
@@ -10,13 +11,39 @@ using namespace mlir;
 
 namespace lazycst {
 
-mlir::StringAttr LazyFunctionManager::nextName(mlir::ModuleOp module) {
+StringAttr LazyFunctionManager::nextName(ModuleOp module) {
   unsigned subscript = counter++;
-  auto name = mlir::StringAttr::get(
-      module.getContext(), "lazycst." + llvm::Twine(subscript));
+  auto name =
+      StringAttr::get(module.getContext(), "lazycst." + Twine(subscript));
   assert(!SymbolTable::lookupSymbolIn(module, name) &&
          "next LazyFuncOp name was already taken");
   return name;
+}
+
+ArrayRef<Attribute> LazyFunctionManager::getResults(
+    const LazyFuncOp &lazyFunction) {
+  // lazyFunction is passed by reference so its type can be forward declared to
+  // avoid cyclical header file dependencies, but its methods are non-const so
+  // we copy the function here to access its information most conveniently
+  LazyFuncOp f = lazyFunction;
+  ArrayRef<Type> resultTypes = f.getResultTypes();
+  unsigned numResults = resultTypes.size();
+  assert(numResults > 0);
+  std::vector<Attribute> *attrs = nullptr;
+  {
+    std::unique_lock<std::mutex> lock(resultsMutex);
+    std::vector<Attribute> nullAttrs(numResults, nullptr);
+    auto [iter, inserted] = results.try_emplace(f.getSymName(), nullAttrs);
+    attrs = &iter->second;
+    if (!inserted) {
+      assert(attrs->size() == numResults);
+      resultsCondition.wait(
+          lock, [attrs] { return attrs->front() != nullptr; });
+      return *attrs;
+    }
+  }
+  // We must calculate results and populate attrs.
+  llvm_unreachable("TODO");
 }
 
 } // namespace lazycst
--- src/Dialect/LazyCst/LazyFunctionManager.hpp
+++ src/Dialect/LazyCst/LazyFunctionManager.hpp
@@ -4,20 +4,36 @@
 
 #pragma once
 
+#include "mlir/IR/Attributes.h"
 #include "mlir/IR/BuiltinAttributes.h"
 #include "mlir/IR/BuiltinOps.h"
 
+#include "llvm/ADT/ArrayRef.h"
+#include "llvm/ADT/StringMap.h"
+
 #include <atomic>
+#include <condition_variable>
+#include <mutex>
+#include <vector>
 
 namespace lazycst {
+
+class LazyFuncOp;
 
 class LazyFunctionManager {
 public:
   LazyFunctionManager() : counter(0) {}
+
   mlir::StringAttr nextName(mlir::ModuleOp module);
+
+  llvm::ArrayRef<mlir::Attribute> getResults(const LazyFuncOp &lazyFunction);
 
 private:
   std::atomic<unsigned> counter;
+
+  std::mutex resultsMutex;
+  std::condition_variable resultsCondition;
+  llvm::StringMap<std::vector<mlir::Attribute>> results;
 };
 
 } // namespace lazycst
