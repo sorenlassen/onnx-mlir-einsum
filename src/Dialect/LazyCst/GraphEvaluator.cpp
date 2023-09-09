45b2ae6852f02349ba5a8633a48313d1c2ccbb04
Soren Lassen <slassen@groq.com>
2023-09-08 17:09:27 -0700

    GraphEvaluator - yet more WIP
    Signed-off-by: Soren Lassen <slassen@groq.com>

--- src/Dialect/LazyCst/GraphEvaluator.cpp
+++ src/Dialect/LazyCst/GraphEvaluator.cpp
@@ -12,39 +12,39 @@ GraphEvaluator::GraphEvaluator(llvm::ThreadPool *threadPool)
 GraphEvaluator::~GraphEvaluator() = default;
 
 // All predecessors must have been added beforehand.
-void GraphEvaluator::addNode(mlir::Operation *node,
-    llvm::ArrayRef<mlir::Operation *> predecessors, Eval eval) {
+void GraphEvaluator::addNode(mlir::Operation *op,
+    llvm::ArrayRef<NodeOperand> operands, Eval eval, bool usedOutsideGraph) {
   assert(eval != nullptr);
-  auto [it, inserted] = nodes.try_emplace(node);
+  auto [it, inserted] = nodes.try_emplace(op);
   assert(inserted);
   OpRecord &rec = it->second;
-  rec.predecessors.reserve(predecessors.size());
-  for (mlir::Operation *predecessor : predecessors)
-    rec.predecessors.push_back(lookup(predecessor));
+  rec.operands.reserve(operands.size());
+  for (auto [op, index] : operands)
+    rec.operands.emplace_back(lookup(op), index);
   rec.eval = std::move(eval);
 }
 
-auto GraphEvaluator::lookup(mlir::Operation *node) -> OpEntry * {
-  auto it = nodes.find(node);
+auto GraphEvaluator::lookup(mlir::Operation *op) -> OpEntry * {
+  auto it = nodes.find(op);
   assert(it != nodes.end());
   return &*it;
 }
 
 void GraphEvaluator::enqueue(OpEntry *entry) {
-  OpRecord &rec = entry->second;
-  if (threadPool) {
-    auto f = [this, entry, eval = std::move(rec.eval)] {
-      eval(entry->first);
-      entry->second.who = 0;
-      condition.notify_all();
-    };
-    assert(rec.eval == nullptr);
-    llvm_unreachable("TODO: enqueue f in thread pool");
-  } else {
-    rec.eval(entry->first);
-    rec.eval = nullptr;
-    rec.who = 0;
+  auto doEval = [this, entry, eval = std::move(entry->second.eval)] {
+    OpRecord &rec = entry->second;
+    llvm::SmallVector<mlir::Attribute> operands;
+    for (auto [node, index] : rec.operands)
+      operands.push_back(node->second.results[index]);
+    eval(entry->first, operands, rec.results);
+    entry->second.who = 0;
     condition.notify_all();
+  };
+  assert(entry->second.eval == nullptr);
+  if (threadPool) {
+    llvm_unreachable("TODO: enqueue doEval in thread pool");
+  } else {
+    doEval();
   }
 }
 
@@ -62,8 +62,8 @@ bool GraphEvaluator::tryEvaluateNode(OpEntry *entry, int me, Set &awaiting) {
   }
   rec.who = me;
   bool queue = true;
-  for (OpEntry *predecessor : rec.predecessors)
-    queue &= tryEvaluateNode(predecessor, me, awaiting);
+  for (auto [node, _] : rec.operands)
+    queue &= tryEvaluateNode(node, me, awaiting);
   if (queue) {
     enqueue(entry);
     if (rec.isEvaluated())
@@ -74,20 +74,23 @@ bool GraphEvaluator::tryEvaluateNode(OpEntry *entry, int me, Set &awaiting) {
   return false;
 }
 
-void GraphEvaluator::evaluate(llvm::ArrayRef<mlir::Operation *> nodes) {
+void GraphEvaluator::evaluate(llvm::ArrayRef<mlir::Operation *> ops,
+    llvm::SmallVectorImpl<llvm::ArrayRef<mlir::Attribute>> &results) {
   std::unique_lock<std::mutex> lock(mux);
   int me = ++whoCounter;
   assert(me != 0);
   Set awaiting;
-  for (auto node : nodes)
-    tryEvaluateNode(lookup(node), me, awaiting);
+  llvm::SmallVector<OpEntry *> nodes(
+      llvm::map_range(ops, [this](mlir::Operation *op) { return lookup(op); }));
+  for (auto *node : nodes)
+    tryEvaluateNode(node, me, awaiting);
   while (!awaiting.empty()) {
     condition.wait(lock);
     llvm::SmallVector<OpEntry *> evaluated;
     for (OpEntry *entry : awaiting) {
       OpRecord &rec = entry->second;
       if (rec.who == me && rec.eval != nullptr &&
-          llvm::all_of(rec.predecessors, isEvaluated)) {
+          llvm::all_of(rec.operands, isOperandEvaluated)) {
         enqueue(entry);
       }
       if (rec.isEvaluated())
@@ -96,6 +99,8 @@ void GraphEvaluator::evaluate(llvm::ArrayRef<mlir::Operation *> nodes) {
     for (OpEntry *entry : evaluated)
       awaiting.erase(entry);
   }
+  for (auto *node : nodes)
+    results.push_back(llvm::ArrayRef(node->second.results));
 }
 
 } // namespace lazycst
--- src/Dialect/LazyCst/GraphEvaluator.hpp
+++ src/Dialect/LazyCst/GraphEvaluator.hpp
@@ -4,6 +4,8 @@
 
 #pragma once
 
+#include "mlir/IR/Attributes.h"
+
 #include "llvm/ADT/ArrayRef.h"
 #include "llvm/ADT/STLExtras.h"
 #include "llvm/ADT/SmallPtrSet.h"
@@ -15,6 +17,7 @@
 #include <functional>
 #include <mutex>
 #include <unordered_map>
+#include <utility>
 
 namespace mlir {
 class Operation;
@@ -24,39 +27,54 @@ namespace lazycst {
 
 class GraphEvaluator {
 public:
-  using Eval = std::function<void(mlir::Operation *)>;
+  using NodeOperand = std::pair<mlir::Operation *, unsigned>;
+  using Eval =
+      std::function<void(mlir::Operation *, llvm::ArrayRef<mlir::Attribute>,
+          llvm::SmallVectorImpl<mlir::Attribute> &results)>;
 
   GraphEvaluator(llvm::ThreadPool *threadPool);
 
   ~GraphEvaluator();
 
-  // All predecessors must have been added beforehand.
-  void addNode(mlir::Operation *node,
-      llvm::ArrayRef<mlir::Operation *> predecessors, Eval eval);
+  // All operand ops must have been added beforehand.
+  void addNode(mlir::Operation *op, llvm::ArrayRef<NodeOperand> operands,
+      Eval eval, bool usedOutsideGraph = false);
 
-  void evaluate(llvm::ArrayRef<mlir::Operation *> nodes);
+  void evaluate(llvm::ArrayRef<mlir::Operation *> ops,
+      llvm::SmallVectorImpl<llvm::ArrayRef<mlir::Attribute>> &results);
 
 private:
   struct OpRecord;
+
   using OpEntry = std::pair<mlir::Operation *const, OpRecord>;
+
+  using OpEntryOperand = std::pair<OpEntry *, unsigned>;
+
   struct OpRecord {
-    llvm::SmallVector<OpEntry *, 2> predecessors;
+    llvm::SmallVector<mlir::Attribute, 1> results;
+    llvm::SmallVector<OpEntryOperand> operands;
+    // Add nullptr to users to represent any out-of-graph users.
+    // TODO: consider attaching users to each result instead  of while OpRecord
+    llvm::SmallPtrSet<OpEntry *, 1> users;
     // Function to evaluate.
     // Set to nullptr after it has been queued for evaluation.
     Eval eval;
     // Who is assigned to evaluate. Cleared after completion.
+    // TODO: consider removing atomic and access under mutex, see: t.ly/DdLO3
     std::atomic<int> who = 0;
 
     bool isVisited() const { return eval == nullptr || who != 0; }
     bool isQueued() const { return eval == nullptr && who != 0; }
     bool isEvaluated() const { return eval == nullptr && who == 0; }
   };
+
   using Set = llvm::SmallPtrSet<OpEntry *, 1>;
-  static bool isEvaluated(OpEntry *entry) {
-    return entry->second.isEvaluated();
+
+  static bool isOperandEvaluated(OpEntryOperand operand) {
+    return operand.first->second.isEvaluated();
   }
 
-  OpEntry *lookup(mlir::Operation *node);
+  OpEntry *lookup(mlir::Operation *op);
 
   void enqueue(OpEntry *entry);
 
