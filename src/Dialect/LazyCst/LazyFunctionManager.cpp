0b4f03a0c70dbd565829843bd7f066ee15c5b1a1
Soren Lassen <slassen@groq.com>
2023-09-09 21:46:32 -0700

    LazyArgsConstantFolder
    Signed-off-by: Soren Lassen <slassen@groq.com>

--- src/Dialect/LazyCst/LazyFunctionManager.cpp
+++ src/Dialect/LazyCst/LazyFunctionManager.cpp
@@ -8,6 +8,9 @@
 #include "src/Dialect/LazyCst/LazyCstAttributes.hpp"
 #include "src/Dialect/LazyCst/LazyCstOps.hpp"
 
+#include <algorithm>
+#include <iterator>
+
 using namespace mlir;
 
 namespace lazycst {
@@ -26,30 +29,50 @@ LazyFuncOp LazyFunctionManager::create(SymbolTable &symbolTable, Location loc) {
 
 namespace {
 
+class LazyArgsConstantFolder : public ConstantFolder {
+public:
+  void fold(mlir::Operation *cstexprOp,
+      llvm::ArrayRef<mlir::Attribute> operands,
+      llvm::SmallVectorImpl<mlir::Attribute> &results) const override final {
+    assert(operands.empty());
+    LazyFuncOp cstexpr = cast<LazyFuncOp>(cstexprOp);
+    llvm::copy(cstexpr.getArgConstantsAttr(), std::back_inserter(results));
+  }
+};
+
 class CstExprConstantFolder : public ConstantFolder {
 public:
   void fold(mlir::Operation *cstexprOp,
       llvm::ArrayRef<mlir::Attribute> operands,
       llvm::SmallVectorImpl<mlir::Attribute> &results) const override final {
+    static LazyArgsConstantFolder argsFolder;
     LazyFuncOp cstexpr = cast<LazyFuncOp>(cstexprOp);
+    // TODO: check that all lazy elms args constants are evaluated
+    MLIRContext *ctx = cstexpr.getContext();
     const ConstantFolders &constantFolders =
-        cstexpr.getContext()
-            ->getLoadedDialect<lazycst::LazyCstDialect>()
-            ->constantFolders;
-    // TODO: use LazyFunctionManager thread pool
-    GraphEvaluator cstexprEvaluator(nullptr);
+        ctx->getLoadedDialect<lazycst::LazyCstDialect>()->constantFolders;
+    GraphEvaluator cstexprEvaluator(&ctx->getThreadPool());
+    // cstexprOp is used as a pseudo-op that folds with the args as results.
+    cstexprEvaluator.addNode(cstexprOp, {}, &argsFolder);
     Operation *terminator = cstexpr.getBody().back().getTerminator();
-    assert(terminator->hasOneUse());
-    Operation *resultOp = *terminator->user_begin();
     for (Operation &op : cstexpr.getBody().getOps()) {
       if (&op != terminator) {
         const ConstantFolder *constantFolder =
             constantFolders.lookup(op.getName());
-        llvm::SmallVector<mlir::Attribute> results;
-        SmallVector<GraphEvaluator::NodeOperand> operands; // TODO: populate
+        SmallVector<GraphEvaluator::NodeOperand> operands;
+        for (Value v : op.getOperands()) {
+          if (auto r = dyn_cast<OpResult>(v))
+            operands.emplace_back(r.getOwner(), r.getResultNumber());
+          else if (auto a = dyn_cast<BlockArgument>(v))
+            operands.emplace_back(cstexprOp, a.getArgNumber());
+          else
+            llvm_unreachable("Value is OpResult or BlockArgument");
+        }
         cstexprEvaluator.addNode(&op, operands, constantFolder);
       }
     }
+    Operation *resultOp = terminator->getResult(0).getDefiningOp();
+    assert(resultOp != nullptr);
     SmallVector<ArrayRef<Attribute>, 1> attrs;
     cstexprEvaluator.evaluate({resultOp}, attrs);
     ArrayRef<Attribute> resultAttrs = attrs.front();
@@ -69,8 +92,7 @@ void LazyFunctionManager::record(
       operands.emplace_back(callee, lazyElms.getIndex());
     }
   }
-  evaluator.addNode(
-      cstexpr, operands, &folder, onlyLazyFunctionUsers);
+  evaluator.addNode(cstexpr, operands, &folder, onlyLazyFunctionUsers);
 }
 
 Attribute LazyFunctionManager::getResult(LazyFuncOp cstexpr, unsigned index) {
